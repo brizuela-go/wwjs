c21a786e89043fca81907c4423464b27b0670e23
Juan Brizuela <73278498+brizuela-go@users.noreply.github.com>
2024-05-18 21:25:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const client = new Client({
   puppeteer: {
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
-});
+  webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+}});
 
 const OpenAI = require("openai");
 
